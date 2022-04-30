a07f8f2fc6117c661255d925b07fd500b13fd5bf
rafaelmyb <rafaelyamada321@gmail.com>
2022-04-30 11:26:31 -0400

    add splash & signIn screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
-import { View, Text, Image } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StatusBar
+} from "react-native";
+
+import { ButtonIcon } from "../../components/ButtonIcon";
 
 import illustrationImg from '../../assets/illustration.png';
+
 import { styles } from "./styles";
 
 export function SignIn() {
   return (
     <View style={styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       <Image
         source={illustrationImg}
         style={styles.image}
+        resizeMode="stretch"
       />
 
       <View style={styles.content}>
@@ -24,8 +38,11 @@ export function SignIn() {
           favoritos com seus amigos
         </Text>
 
+        <ButtonIcon
+          title="Entrar com Discord"
+          activeOpacity={0.7}
+        />
       </View>
-
     </View>
   );
 }
